320a6fe1d5b9f7bd0c23b0beafe3d44cc2419d8a
AstorianHistorian <d.a.mutik@gmail.com>
2022-05-17 18:35:26 +0300

    with blame

--- get-commits.py
+++ get-commits.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 import argparse
 import os
 
@@ -5,27 +6,30 @@ parser = argparse.ArgumentParser(description='Script to find commits.')
 parser.add_argument("--start-rev", type=str, default=1)
 parser.add_argument("--end-rev", type=str, default='HEAD')
 parser.add_argument("--svn-repo-path", type=str, default='https://svn.code.sf.net/p/codeblocks/code/trunk')
+parser.add_argument("--file-name", type=str, default='')
 args = parser.parse_args()
-opt1_value = args.start_rev
-opt2_value = args.end_rev
-opt3_value = args.svn_repo_path
+opt_value1 = args.start_rev
+opt_value2 = args.end_rev
+opt_value3 = args.svn_repo_path
+opt_value4 = args.file_name
 
-line = f"svn log -r {opt1_value}:{opt2_value} {opt3_value} -l 15"
+line = f"svn log -r {opt_value1}:{opt_value2} {opt_value3} {opt_value4} -l 15"
 
 def write():
-    with open('//home/dmitry/Desktop/output.log','r') as f:
-        ln = f.readlines()
-    
-    with open('//home/dmitry/Desktop/output.log','w') as f:
-        for line in ln:
-            if "|" in line:
-                line = line.split("|")
-                f.write(f"@{line[1]}\n")
-            elif ("---" in line) or (line == '\n'):
-                pass
-            else:
-                f.write(f"  -{line}")
+        with open('//home/dmitry/Desktop/output.log','r') as f:
+            ln = f.readlines()
 
+        with open('//home/dmitry/Desktop/output.log','w') as f:
+            for line in ln:
+                if "|" in line:
+                    line = line.split("|")
+                    f.write(f"@{line[1]}\n")
+                elif ("---" in line) or (line == '\n'):
+                    pass
+                else:
+                    f.write(f"  -{line}")
 
+if opt_value4 != '':
+    os.system(f"svn blame {opt_value3}> //home/dmitry/Desktop/outputfile.log")
 os.system(f'{line} > //home/dmitry/Desktop/output.log')
 write()
