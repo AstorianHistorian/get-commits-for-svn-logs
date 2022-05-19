88f28e206051ef898a884669940f31fbccb4c1be
AstorianHistorian <97513774+AstorianHistorian@users.noreply.github.com>
2022-05-19 13:29:09 +0300

    Minor cleanup
    fixed default values

--- get-commits.py
+++ get-commits.py
@@ -3,14 +3,14 @@ import argparse
 import os
 from pathlib import Path
 
-df = "*.txt*"
+
 home = str(Path.home())
 
 parser = argparse.ArgumentParser(description='Script to find commits.')
 parser.add_argument("--start-rev", type=str, default=1)
 parser.add_argument("--end-rev", type=str, default='HEAD')
 parser.add_argument("--svn-repo-path", type=str, default='http://svn.code.sf.net/p/keepass/code/trunk/')
-parser.add_argument("--file-name", type=str, default=f'{df}')
+parser.add_argument("--file-name", type=str, default='')
 args = parser.parse_args()
 opt_value1 = args.start_rev
 opt_value2 = args.end_rev
@@ -18,10 +18,10 @@ opt_value3 = args.svn_repo_path
 opt_value4 = args.file_name
 
 def write():
-        with open(f'/{home}/Desktop/logs/output.log','r') as f:
+        with open(f'/{home}/Desktop/output.txt','r') as f:
             ln = f.readlines()
 
-        with open(f'/{home}/Desktop/logs/output.log','w') as f:
+        with open(f'/{home}/Desktop/output.txt','w') as f:
             for line in ln:
                 if "|" in line:
                     line = line.split("|")
@@ -31,10 +31,8 @@ def write():
                 else:
                     f.write(f"  -{line}")
 
-os.system("mkdir $HOME/Desktop/logs/")
-
 if opt_value4 != '':
-    os.system(f"svn log --search {opt_value4} -r {opt_value1}:{opt_value2} {opt_value3} > $HOME/Desktop/logs/output.log")
+    os.system(f"svn log --search {opt_value4} -r {opt_value1}:{opt_value2} {opt_value3} > $HOME/Desktop/output.txt")
 else:    
-    os.system(f"svn log -r {opt_value1}:{opt_value2} {opt_value3} > $HOME/Desktop/logs/output.log")
+    os.system(f"svn log -r {opt_value1}:{opt_value2} {opt_value3} > $HOME/Desktop/output.txt")
 write()
